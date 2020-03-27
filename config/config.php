07b14d1a06fc55746093492b6c8dc5570297fb2c
shijie.chen <shijie.chen_c@chinapnr.com>
2020-03-27 18:40:45 +0800

    20200327 init-2

--- config/config.php
+++ config/config.php
@@ -4,5 +4,4 @@
 $route_conf = array(
     ['method' => 'GET',  'uri' => '/index',          'action' => 'IndexController.index'],
     ['method' => 'GET',  'uri' => '/',               'action' => 'IndexController.index'],
-    ['method' => 'POST', 'uri' => '/index/register', 'action' => 'IndexController.register'],
 );
