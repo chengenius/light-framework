515c37dab9d358c32f36a5854c796df44c3a4529
shijie.chen <shijie.chen_c@chinapnr.com>
2020-03-28 16:59:35 +0800

    20200328 project init

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+//'GET', '/index/index', 'IndexController.index'
+$route_conf = array(
+    ['method' => 'GET',  'uri' => '/index',          'action' => 'IndexController.index'],
+    ['method' => 'GET',  'uri' => '/',               'action' => 'IndexController.index'],
+);
